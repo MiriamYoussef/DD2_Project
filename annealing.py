5e652698a69f5c7fb54d2f9ac1629de358909414
mhassan20328 <60842042+mhassan20328@users.noreply.github.com>
2022-11-23 21:59:35 +0200

    Final Version

--- annealing.py
+++ annealing.py
@@ -1,16 +1,23 @@
 import numpy as np
 import random
 import re
-
+import math
 print("Simulated Annealing Project")
 def print_b(board):
     print(np.matrix(board))
-# def makenp(board):
-#     return np.matrix(board)
+
+
+
+
 
 def initialise_array(numrows,numcols):
     board = [['---']*numcols for i in range(numrows)]
     return board
+
+def moves(elements):
+    iterations=10*elements
+    return iterations
+
 def checkboardfull(board):
     if any('---' in nested_list for nested_list in board):
         return False
@@ -24,10 +31,7 @@ def calcdistance(board,connectionlist):
     for elements in connectionlist:
         for element in elements:
             result = np.argwhere(board == (element))
-            # print("Element to find: ")
-            # print(element)
-            # print("this is result")
-            # print(result)
+
             currentrow=result[0][0]
             currentcol=result[0][1]
             for index, number in enumerate(elements):
@@ -42,28 +46,58 @@ def calcdistance(board,connectionlist):
                     max_col=y
                 z=max_col+max_row
         lengthlist.append(z)
-    # print("Length List=")            
-    # print(lengthlist)
-    # print(len(lengthlist))
-    # print("Sum of length list")
-    # print(sum(lengthlist))
     return sum(lengthlist)
-    
-f = open("d2.txt", "r")
+
+
+
+
+def thermal_an(board,connectionlist,intial,numnets,numcells):
+    inital_temp=intial*500
+    final_temp=(5*math.pow(10,-6))*(intial/int(numnets))
+    print(final_temp)
+    temper=inital_temp
+    moves=10*int(numcells)
+    print(moves)
+    iterations=0
+    while(temper>final_temp):
+        randr1=random.randint(0, numrows-1)
+        randc1=random.randint(0, numcols-1)
+        randr2=random.randint(0, numrows-1)
+        randc2=random.randint(0, numcols-1)
+        board[randr1][randc1], board[randr2][randc2]= board[randr2][randc2], board[randr1][randc1]
+        iterations=iterations+1
+        prop=calcdistance(board,connectionlist)
+        delta_l=prop-intial
+        propability = math.exp(-delta_l/temper)
+        if (delta_l<0) or random.uniform(0, 1)<propability :
+            intial=prop
+        if iterations==moves:
+            temper=temper*0.95
+            iterations=0
+        print(str(temper) +'/'+str(final_temp))
+    print("FINAL PLACEMENT")
+    print(board)
+    print("FINAL LENGTH")
+    print(intial)
+        
+filename=input("Please enter the name of the file with the .txt extension: ")
+f = open(filename, "r")
 # print(f.read()) 
 fstline=f.readline()
 numsinfstline=re.findall(r'\d+', fstline)
 print("Getting number of cells")
 numcells=numsinfstline[0]
 print(numcells)
-print("Getting number of connections")
-numconnections=numsinfstline[1]
-print(numconnections)
+print("Getting number of nets")
+numnets=numsinfstline[1]
+print(numcells)
 print("getting number of rows and cols")
 numrows=int(numsinfstline[2])
 numcols=int(numsinfstline[3])
 print(numrows)
 print(numcols)
+f.close
+
 
 
 
@@ -94,8 +128,8 @@ for item in list_of_comp:
 # print(clean)
 #now we need to remove duplicates
 mylist = list(dict.fromkeys(clean))
-print("list of components")
-print(mylist)
+# print("list of components")
+# print(mylist)
 for index, element in enumerate(mylist):
     randomrow=random.randint(0, numrows)
     randomcol=random.randint(0, numcols)
@@ -120,6 +154,10 @@ for index, x in enumerate(list_of_comp):
 # print(connectionlist[7])
 
 intial=calcdistance(board,connectionlist)
-print("wire length is:")
+print("initial wire length is:")
 print(intial)
-#Testing
+#Annealing
+thermal_an(board,connectionlist,intial,numnets,numnets)
+
+
+
